169514802b3cca789fd653c9339ecfb6ba5062e7
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-02-17 05:42:01 +0100

    :rocket: :tada: create the data for accordion  :rocket: :tada:

---
+++ src/components/accordion/data.js
@@ -0,0 +1,24 @@
+const data =[
+    {
+        id:"1",
+        question: "Quel est le plus grand océan du monde ?",
+        answer: " L'océan Pacifique est le plus grand océan en termes de superficie, couvrant environ un tiers de la surface terrestre.",
+    },
+    {
+        id:"2",
+        question: "Qui est l'auteur de 'Le Petit Prince' ?",
+        answer: "Antoine de Saint-Exupéry est l'auteur de ce célèbre livre publié en 1943.",
+    },
+    {
+        id:"3",
+        question: "Quel est le plus long fleuve du monde et à travers combien de pays s'étend-il ?",
+        answer: "Le plus long fleuve du monde est l'Amazone, qui traverse neuf pays d'Amérique du Sud.",
+    },
+    {
+        id:"4",
+        question: "Qui a inventé la théorie de la relativité et quelle est l'équation la plus célèbre qui en découle ?",
+        answer: "Albert Einstein est l'auteur de la théorie de la relativité, et son équation la plus célèbre est E=mc².",
+    },
+    
+]
+export default data;
