e04843a1e58ea031233f9ee4871dc8863ade8e00
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-02-17 06:21:45 +0100

    :rocket: :tada: create the ui question answer  :rocket: :tada:

--- src/components/accordion/index.jsx
+++ src/components/accordion/index.jsx
@@ -5,19 +5,25 @@ export const Accordion = () => {
   const [selected, setSelected] = useState(null);
 
   const handleSingleSelection = (getCurrentId) => {
-    console.log(getCurrentId);
+    setSelected(getCurrentId);
   };
-
+  console.log(selected);
   return (
     <div className="wrapper">
       <div className="accordion">
         {data && data.length > 0 ? (
           data.map((dataItem) => (
             <div className="item">
-              <div onClick={() => handleSingleSelection(dataItem.id)} className="title">
+              <div
+                onClick={() => handleSingleSelection(dataItem.id)}
+                className="title"
+              >
                 <h3>{dataItem.question}</h3>
                 <span>+</span>
               </div>
+              {selected === dataItem.id ? (
+                <div className="content">{dataItem.answer}</div>
+              ) : null}
             </div>
           ))
         ) : (
